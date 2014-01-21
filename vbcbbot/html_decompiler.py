6864b1d15e1c864287d476941f2f6f318e71eb68
Ondřej Hošek <ondra.hosek@gmail.com>
2014-01-22 00:42:10 +0100

    html_decompiler: implement a lot more tags

--- vbcbbot/html_decompiler.py
+++ vbcbbot/html_decompiler.py
@@ -2,6 +2,7 @@ import logging
 import re
 
 __author__ = 'ondra'
+youtube_embed_re = re.compile("^//www\\.youtube\\.com/([a-zA-Z0-9]+)\\?wmode=opaque$")
 
 logger = logging.getLogger("vbcbbot.html_decompiler")
 
@@ -42,6 +43,17 @@ class Element(Node):
     @staticmethod
     def is_element():
         return True
+
+
+class ListItem(Node):
+    def __init__(self, children):
+        self.children = children
+
+    def __str__(self):
+        return "[*]" + "".join([str(child) for child in self.children])
+
+    def __repr__(self):
+        return "ListItem({0})".format(repr(self.children))
 
 
 class Text(Node):
@@ -126,27 +138,113 @@ class HtmlDecompiler:
                         ret.append(Element("icon", [Text(child.attrs['src'])]))
 
                 elif child.name == "a" and child.has_attr("href"):
-                    child_list = list(child.children)
-                    if len(child_list) == 1 and child_list[0].name == "img" and \
-                            child_list[0].has_attr("src") and \
-                            child_list[0]['src'] == child['href']:
-                        # icon -- let the img handler take care of it
-                        ret += self.decompile_soup(child)
+                    if child.attrs["href"].startswith("mailto:"):
+                        # e-mail link
+                        address = child.attrs["href"][len("mailto:"):]
+                        ret.append(Element("email", self.decompile_soup(child), address))
                     else:
-                        # some other link -- do it manually
-                        ret.append(Element("url", self.decompile_soup(child), child["href"]))
+                        child_list = list(child.children)
+                        if len(child_list) == 1 and child_list[0].name == "img" and \
+                                child_list[0].has_attr("src") and \
+                                child_list[0]['src'] == child.attrs['href']:
+                            # icon -- let the img handler take care of it
+                            ret += self.decompile_soup(child)
+                        else:
+                            # some other link -- do it manually
+                            # this also catches [thread], [post], [rtfaq], [stfw] -- the difference
+                            # to normal [url] is so minimal I decided not to implement it
+                            ret.append(Element("url", self.decompile_soup(child),
+                                               child.attrs["href"]))
 
                 elif child.name in "biu":
                     # bold/italic/underline!
                     ret.append(Element(child.name, self.decompile_soup(child)))
+                elif child.name == "sub":
+                    ret.append(Element("t", self.decompile_soup(child)))
+                elif child.name == "sup":
+                    ret.append(Element("h", self.decompile_soup(child)))
+                elif child.name == "strike":
+                    ret.append(Element("strike", self.decompile_soup(child)))
 
                 elif child.name == "font" and child.has_attr("color"):
                     # font color
-                    ret.append(Element("color", self.decompile_soup(child), child['color']))
+                    ret.append(Element("color", self.decompile_soup(child), child.attrs['color']))
 
                 elif child.name == "span" and child.has_attr("style"):
-                    if child["style"] == "direction: rtl; unicode-bidi: bidi-override;":
+                    if child.attrs["style"] == "direction: rtl; unicode-bidi: bidi-override;":
                         ret.append(Element("flip", self.decompile_soup(child)))
+                    elif child.attrs["style"].startswith("font-family: "):
+                        font_family = child.attrs["style"][len("font-family: "):]
+                        ret.append(Element("font", self.decompile_soup(child), font_family))
+
+                elif child.name == "span" and child.has_attr("class"):
+                    if child.attrs["class"] == "highlight":
+                        ret.append(Element("highlight", self.decompile_soup(child)))
+                    elif child.attrs["class"] == "IRONY":
+                        ret.append(Element("irony", self.decompile_soup(child)))
+
+                elif child.name == "div" and child.has_attr("style"):
+                    if child.attrs["style"] == "margin-left:40px":
+                        ret.append(Element("indent", self.decompile_soup(child)))
+                    elif child.attrs["style"] == "text-align: left;":
+                        ret.append(Element("left", self.decompile_soup(child)))
+                    elif child.attrs["style"] == "text-align: center;":
+                        ret.append(Element("center", self.decompile_soup(child)))
+                    elif child.attrs["style"] == "text-align: right;":
+                        ret.append(Element("right", self.decompile_soup(child)))
+
+                elif child.name == "div" and child.has_attr("class"):
+                    if child.attrs["class"] == "bbcode_container":
+                        code_pre = child.find("pre", attrs={"class": "bbcode_code"},
+                                              recursive=False)
+                        quote_div = child.find("div", attrs={"class": "bbcode_quote"},
+                                               recursive=False)
+                        if code_pre is not None:
+                            # [code]
+                            # FIXME: is this correct (enough)?
+                            code_string = child.find("pre").text
+                            ret.append(Element("code", [Text(code_string)]))
+                        elif quote_div is not None:
+                            # [quote]
+                            # find poster
+                            post_number = None
+                            poster_name = None
+                            posted_by_div = quote_div.find("div",
+                                                           attrs={"class": "bbcode_postedby"})
+                            if posted_by_div is not None:
+                                poster_name = posted_by_div.find("strong").text
+                                poster_link_a = posted_by_div.find("a", href=True)
+                                if poster_link_a is not None and \
+                                        poster_link_a.attrs["href"].startswith("showthread.php?p="):
+                                    post_href_rest = \
+                                        poster_link_a.attrs["href"][len("showthread.php?p="):]
+                                    post_number = post_href_rest[:post_href_rest.find("#")]
+
+                            quote_attrib = None
+                            if poster_name is not None:
+                                quote_attrib = poster_name
+                                if post_number is not None:
+                                    quote_attrib += ";{0}".format(post_number)
+
+                            message_div = quote_div.find("div", attrs={"class": "message"})
+
+                            ret.append(Element("quote", self.decompile_soup(message_div),
+                                               quote_attrib))
+
+                elif child.name == "ul":
+                    ret.append(Element("list", self.decompile_soup(child)))
+                elif child.name == "ol" and child.has_attr("class") and \
+                        child.attrs["class"] == "decimal":
+                    ret.append(Element("list", self.decompile_soup(child), "1"))
+                elif child.name == "li" and child.has_attr("style") and child.attrs["style"] == "":
+                    ret.append(ListItem(self.decompile_soup(child)))
+
+                elif child.name == "iframe" and child.has_attr("src"):
+                    match = youtube_embed_re.match(child.attrs["src"])
+                    if match is not None:
+                        # YouTube embed
+                        video_selector = "youtube;" + match.group(1)
+                        ret.append(Element("video", [Text("a video")], video_selector))
 
                 else:
                     logger.warning("skipping unknown HTML element {0}".format(child.name))
