1a8719a1361365e1373eab066f28f0fdcf6889c5
Ondřej Hošek <ondra.hosek@gmail.com>
2014-05-01 00:35:57 +0200

    last_seen_api: allow multi-nick requests

--- vbcbbot/modules/last_seen_api.py
+++ vbcbbot/modules/last_seen_api.py
@@ -30,53 +30,89 @@ class LastSeenApi(Module):
         if match is None:
             return
 
-        nickname = match.group(2).strip()
-        url_escaped_nickname = up.quote_plus(nickname)
+        nicknames = [nick.strip().replace("[/noparse]", "") for nick in match.group(2).split(";")]
+        nicknames_times = {}
 
-        # note: must be specified as "%%USERNAME%%" in the config file
-        # due to variable substitution in configparse
-        call_url = self.api_url.replace("%USERNAME%", url_escaped_nickname)
-
-        request = ur.Request(call_url)
-
-        # authenticate?
-        if self.api_username != "":
-            authentication_pair = "{0}:{1}".format(self.api_username, self.api_password)
-            authentication_bytes = authentication_pair.encode("utf-8")
-            authentication_b64_string = base64.b64encode(authentication_bytes).decode("us-ascii")
-            request.add_header("Authorization", "Basic {0}".format(authentication_b64_string))
-
-        response = ur.urlopen(request)
-        response_data = response.read().decode("us-ascii")
-
-        if response_data == "NULL":
-            self.connector.send_message(
-                "{0}: The great and powerful [i]signanz[/i] doesn't remember seeing "
-                "[i]{1}[/i].".format(
-                    message.user_name,
-                    self.connector.escape_outgoing_text(nickname)
-                )
-            )
+        if len(nicknames) == 0:
+            return
+        if len(nicknames) == 1 and len(nicknames[0]) == 0:
             return
 
-        try:
-            timestamp = int(response_data)
-        except ValueError:
+        for nickname in nicknames:
+            url_escaped_nickname = up.quote_plus(nickname)
+
+            # note: must be specified as "%%USERNAME%%" in the config file
+            # due to variable substitution in configparse
+            call_url = self.api_url.replace("%USERNAME%", url_escaped_nickname)
+
+            request = ur.Request(call_url)
+
+            # authenticate?
+            if self.api_username != "":
+                authentication_pair = "{0}:{1}".format(self.api_username, self.api_password)
+                authentication_bytes = authentication_pair.encode("utf-8")
+                authentication_b64_string = base64.b64encode(authentication_bytes).decode("us-ascii")
+                request.add_header("Authorization", "Basic {0}".format(authentication_b64_string))
+
+            response = ur.urlopen(request)
+            response_data = response.read().decode("us-ascii")
+
+            if response_data == "NULL":
+                nicknames_times[nickname] = None
+                continue
+
+            try:
+                timestamp = int(response_data)
+            except ValueError:
+                nicknames_times[nickname] = -1
+                continue
+
+            nicknames_times[nickname] = time.strftime("%Y-%m-%d %H:%M", time.localtime(timestamp))
+
+        if len(nicknames) == 1:
+            # single-user request
+            nickname = nicknames[0]
+            t = nicknames_times[nickname]
+
+            if t is None:
+                self.connector.send_message(
+                    "{0}: The great and powerful [i]signanz[/i] doesn't remember seeing "
+                    "[i]{1}[/i].".format(
+                        message.user_name,
+                        self.connector.escape_outgoing_text(nickname)
+                    )
+                )
+            elif t == -1:
+                self.connector.send_message(
+                    "{0}: The great and powerful [i]signanz[/i]'s answer confused me\u2014sorry!".format(
+                        message.user_name
+                    )
+                )
+            else:
+                self.connector.send_message(
+                    "{0}: The last time the great and powerful [i]signanz[/i] saw "
+                    "[i][noparse]{1}[/noparse][/i] was {2}.".format(
+                        message.user_name, nickname, t
+                    )
+                )
+        else:
+            response_bits = []
+            for nickname in nicknames:
+                t = nicknames_times[nickname]
+
+                if t is None:
+                    t = "never"
+                elif t == -1:
+                    t = "o_O"
+
+                response_bits.append("[i][noparse]{0}[/noparse][/i]: {1}".format(nickname, t))
+
             self.connector.send_message(
-                "{0}: The great [i]signanz[/i]'s answer confused me\u2014sorry!".format(
-                    message.user_name
+                "{0}: The great and powerful [i]signanz[/i] saw: ".format(message.user_name) +
+                ", ".join(
+                    response_bits
                 )
             )
-            return
-
-        time_text = time.strftime("%Y-%m-%d %H:%M", time.localtime(timestamp))
-
-        self.connector.send_message(
-            "{0}: The last time the great and powerful [i]signanz[/i] saw "
-            "[i][noparse]{1}[/noparse][/i] was {2}.".format(
-                message.user_name, nickname, time_text
-            )
-        )
 
     def __init__(self, connector, config_section):
         """
